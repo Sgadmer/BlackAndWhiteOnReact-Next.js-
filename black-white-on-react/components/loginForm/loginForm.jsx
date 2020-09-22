44b5dec382d559a0761a2c364fcf101c8c69e9e0
Sgadmer <r.smim@yandex.ru>
2020-09-22 21:20:58 +0400

    Исправил постоянное обновление стейта через useEffect

--- black-white-on-react/components/loginForm/loginForm.jsx
+++ black-white-on-react/components/loginForm/loginForm.jsx
@@ -1,75 +1,72 @@
-import { useState, useEffect } from 'react';
-import Router from 'next/router'
-import classes from '../../styles/index.module.scss'
-import NameInput from './nameInput.jsx'
-import SubmitBTN from './submitBTN';
-import InputError from './inputError';
-import { objectToURL } from '../encodeDecodeURL/encodeDecodeURL.js'
-import connectSocket from '../socket/socket';
-
+import { useState, useEffect } from "react";
+import Router from "next/router";
+import classes from "../../styles/index.module.scss";
+import NameInput from "./nameInput.jsx";
+import SubmitBTN from "./submitBTN";
+import InputError from "./inputError";
+import { objectToURL } from "../encodeDecodeURL/encodeDecodeURL.js";
+import connectSocket from "../socket/socket";
 
 export default function LoginForm({ userData }) {
+  const [errorText, setErrorText] = useState("");
+  const [input, setInput] = useState("");
+  const [submitBTNText, setsubmitBTNText] = useState("");
 
-    const [errorText, setErrorText] = useState('');
-    const [input, setInput] = useState("");
-    const [submitBTNText, setsubmitBTNText] = useState("");
-
-
-    useEffect(() => {
-        if (!userData.roomId) {
-            setsubmitBTNText('Начать игру')
-        } else {
-            setsubmitBTNText('Присоединиться')
-        }
-    })
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        let inputValue = e.target.elements[0].value;
-        if (inputValue.length < 3) {
-            setErrorText('Не менее 3 символов');
-            return;
-        }
-        else {
-            setErrorText('');
-            userData.name = input;
-
-
-            if (!userData.roomId) {
-                let URLToLeaderPage = `/LeadersChoice/${objectToURL(userData)}`;
-                Router.push('/LeadersChoice/[user]', URLToLeaderPage);
-            } else {
-
-                let socket = connectSocket();
-
-
-                setErrorText('Проверяем имя');
-                socket.emit('checkNameOnBusy', userData.roomId, userData.name);
-                socket.on('checkNameRes', ({ res }) => {
-                    if (res) {
-                        setErrorText('Имя занято');
-                    } else {
-                        setErrorText('');
-                        let URLToGamePage = `/Game/${objectToURL(userData)}`;
-                        Router.push('/Game/[user]', URLToGamePage);
-                    }
-                })
-            }
-
-        }
+  useEffect(() => {
+    if (!userData.roomId) {
+      setsubmitBTNText("Начать игру");
+    } else {
+      setsubmitBTNText("Присоединиться");
     }
+    console.log(`Changed`);
+  }, [userData.roomId]); //Вариант с [] в официальной документации считают не очень хорошим, поэтому оставил [userData.roomId]
 
-    function handleChange() {
-        setErrorText('');
+  function handleSubmit(e) {
+    e.preventDefault();
+    let inputValue = e.target.elements[0].value;
+    if (inputValue.length < 3) {
+      setErrorText("Не менее 3 символов");
+      return;
+    } else {
+      setErrorText("");
+      userData.name = input;
+
+      if (!userData.roomId) {
+        let URLToLeaderPage = `/LeadersChoice/${objectToURL(userData)}`;
+        Router.push("/LeadersChoice/[user]", URLToLeaderPage);
+      } else {
+        let socket = connectSocket();
+
+        setErrorText("Проверяем имя");
+        socket.emit("checkNameOnBusy", userData.roomId, userData.name);
+        socket.on("checkNameRes", ({ res }) => {
+          if (res) {
+            setErrorText("Имя занято");
+          } else {
+            setErrorText("");
+            let URLToGamePage = `/Game/${objectToURL(userData)}`;
+            Router.push("/Game/[user]", URLToGamePage);
+          }
+        });
+      }
     }
+  }
 
-    return (
-        <>
-            <form className={classes.wrapper} onSubmit={(e) => handleSubmit(e)} onChange={() => handleChange()}>
-                <InputError text={errorText} />
-                <NameInput input={input} setInput={setInput} />
-                <SubmitBTN submitBTNText={submitBTNText} />
-            </form >
-        </>
-    )
+  function handleChange() {
+    setErrorText("");
+  }
+
+  return (
+    <>
+      <form
+        className={classes.wrapper}
+        onSubmit={(e) => handleSubmit(e)}
+        onChange={() => handleChange()}
+      >
+        <InputError text={errorText} />
+        <NameInput input={input} setInput={setInput} />
+        <SubmitBTN submitBTNText={submitBTNText} />
+      </form>
+    </>
+  );
 }
