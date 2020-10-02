ae8977176c90299cd517c4d2a0922ce7e993598f
Sgadmer <r.smim@yandex.ru>
2020-10-02 14:05:54 +0400

    1) Добавил иконку сайта 2) Добавил название сайта 3) Переместил все изображения в папку public

--- black-white-on-react/pages/_app.jsx
+++ black-white-on-react/pages/_app.jsx
@@ -1,10 +1,15 @@
 import "../styles/nullStyle.scss";
 import "../styles/app.scss";
 import { SocketProvider } from "../servicesAndUtilities/SocketContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <SocketProvider>
+      <Head>
+        <title>Black and White</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </SocketProvider>
   );
